7589a717d04061d36728217ed13dde8e81cd4162
ashtoncuthcer-cyber <ashtoncuthcer@gmail.com>
2025-12-29 11:09:52 +0000

    .

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
-from .database import create_db_and_tables
+# from .database import create_db_and_tables
 from .routers import post, user, auth, vote
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    print('DB tables created')
+    # create_db_and_tables()
+    # print('DB tables created')
     yield
     print('App shutting down')
 
 app = FastAPI(lifespan=lifespan)
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
